707d3d828cc7469702a551bf1f32770578b22aa7
Jmorenov <jmorenov28@gmail.com>
2015-01-21 19:20:06 +0100

    appFotoBrujula.

--- appFotoBrujula/src/com/NPI_GYMKANA/appfotobrujula/MainActivity.java
+++ appFotoBrujula/src/com/NPI_GYMKANA/appfotobrujula/MainActivity.java
@@ -1,52 +1,270 @@
 package com.NPI_GYMKANA.appfotobrujula;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.hardware.GeomagneticField;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_main);
-    	
-    	LocationManager milocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    	LocationListener milocListener = new MiLocationListener();
-    	milocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, milocListener);
-    	
-    	final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/"; 
-        File newdir = new File(dir); 
-        newdir.mkdirs();
-    	
-    	loc.getLatitude();
-    	loc.getLongitude();
-    	String coordenadas = “Mis coordenadas son: ” + “Latitud = ” + loc.getLatitude() + “Longitud = ” + loc.getLongitude();
-    	Toast.makeText( getApplicationContext(),coordenadas,Toast.LENGTH_LONG).show();
-    	
-    	Button capture = (Button) findViewById(R.id.btnCapture);
-        capture.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+	// TextViews
+	private TextView northDirection;
+	private TextView latitudeText;
+	private TextView longitudeText;
+	private TextView distanceText;
 
-                // here,counter will be incremented each time,and the picture taken by camera will be stored as 1.jpg,2.jpg and likewise.
-                count++;
-                String file = dir+count+".jpg";
-                File newfile = new File(file);
-                try {
-                    newfile.createNewFile();
-                } catch (IOException e) {}       
+	// Create longitude and latitude variables
+	private double latitude;
+	private double longitude;
 
-                Uri outputFileUri = Uri.fromFile(newfile);
+	// Create distance
+	private double distance;
 
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+	// Initialize ImageView
+	private ImageView image;
 
-                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
-            }
-        });
-    	
+	// Degrees
+	private float currentDegree = 0.0f;
+	private float lastDegree = 0.0f;
+
+	// Sensors and location
+	private SensorManager mSensorManager;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private Location lastLocation;
+
+	private boolean brujula_start = false;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+	public void click(View view) {
+		setContentView(R.layout.brujula);
+		brujula_start = true;
+		startBrujula();
+		// startActivity(new Intent(this, CameraActivity.class));
+	}
+
+	public void startBrujula() {
+		// Fill the image with the compass
+		image = (ImageView) findViewById(R.id.imageViewCompass);
+
+		// Initial distance is 0
+		distance = 0;
+
+		// Set other texts
+		northDirection = (TextView) findViewById(R.id.northDirection);
+		latitudeText = (TextView) findViewById(R.id.latitudeText);
+		longitudeText = (TextView) findViewById(R.id.longitudeText);
+		distanceText = (TextView) findViewById(R.id.distanceText);
+
+		// Initialize sensors
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		mSensorManager.registerListener(this,
+		mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	/**
+	 * Method called when the app resumes his activity
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Continue listening the orientation sensor
+		if (brujula_start)
+			mSensorManager
+					.registerListener(this, mSensorManager
+							.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+							SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	/**
+	 * Method called when the app pauses his activity
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		// Stop listening the sensor
+		if (brujula_start)
+			mSensorManager.unregisterListener(this);
+	}
+
+	/**
+	 * Method called when the sensor status changes
+	 */
+	@Override
+    public void onSensorChanged(SensorEvent event) {
+    	if(brujula_start)
+    	{
+        // Get the degree
+        lastDegree = Math.round(event.values[0]);
+        double azimuth = lastDegree;
+        //Log.d("GRADOS: ", ""+lastDegree);
+        Location target = new Location("Target");
+        	target.setLongitude(Location.convert("037:11:49.8"));
+        	target.setLatitude(Location.convert("003:37:27.2"));
+        		GeomagneticField geoField = new GeomagneticField(
+        		             (float) target.getLatitude(),
+        		             (float) target.getLongitude(),
+        		             (float) target.getAltitude(),
+        		             System.currentTimeMillis());
+        		azimuth = geoField.getDeclination(); // converts magnetic north into true north
+        		//double bearing = currentLoc.bearingTo(target); // (it's already in degrees)
+        		double direction = azimuth;
+        		
+        Log.d("DIRECCION", direction+"");
+        // Say if we must turn left or right to go north
+        if (lastDegree > 0 && lastDegree < 180) {
+            northDirection.setText("Turn left");
+        } else {
+            northDirection.setText("Turn right");
+        }
+
+        // Create and exetute rotation animation
+        RotateAnimation rotation;
+        rotation = new RotateAnimation(
+                currentDegree,
+                (float)-direction,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotation.setDuration(300);
+        rotation.setFillAfter(true);
+        image.startAnimation(rotation);
+
+        // Update current degree
+        currentDegree = -lastDegree;
+
+        // Update GPS coordinates
+        updateCoordinates();
+    	}
     }
+
+	/**
+	 * Update the GPS cordinates
+	 */
+	private void updateCoordinates() {
+		// Make a location listener
+		locationListener = new LocationListener() {
+
+			// Aux
+			int i = 0;
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				lastLocation = locationManager
+						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				lastLocation = locationManager
+						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+			}
+
+			/**
+			 * When user location change, we compute distances and update
+			 * coordinates texts
+			 */
+			@Override
+			public void onLocationChanged(Location location) {
+
+				// First time we make lastLocation=location to later compute
+				// distances
+				if (i == 0) {
+					lastLocation = location;
+					i++;
+				}
+
+				// Get location from GPS
+				location = locationManager
+						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+				// Compute distance to lastLocation
+				if (location.distanceTo(lastLocation) > 15.0) {
+					distance += location.distanceTo(lastLocation);
+					distanceText.setText(String.valueOf(distance + " meters"));
+				}
+
+				// Update and show location
+				latitude = location.getLatitude();
+				longitude = location.getLongitude();
+
+				if (latitude > 0) {
+					latitudeText.setText(String.valueOf(latitude) + " N");
+				} else {
+					latitudeText.setText(String.valueOf(-latitude) + " S");
+				}
+
+				if (longitude > 0) {
+					longitudeText.setText(String.valueOf(longitude) + " E");
+				} else {
+					longitudeText.setText(String.valueOf(-longitude) + " W");
+				}
+
+				if (location.distanceTo(lastLocation) > 15.0) {
+					lastLocation = location;
+				}
+			}
+		};
+
+		// Ask GPS updates every 10000 ms
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				10000, 0, locationListener);
+	}
+
+	/**
+	 * Called when the accuracy of the registered sensor has changed.
+	 * <p/>
+	 * <p>
+	 * See the SENSOR_STATUS_* constants in
+	 * {@link android.hardware.SensorManager SensorManager} for details.
+	 *
+	 * @param sensor
+	 *            Sensor type
+	 * @param accuracy
+	 *            The new accuracy of this sensor, one of
+	 *            {@code SensorManager.SENSOR_STATUS_*}
+	 */
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
 }
---
+++ appFotoBrujula/src/com/NPI_GYMKANA/appfotobrujula/Preview.java
@@ -0,0 +1,103 @@
+package com.NPI_GYMKANA.appfotobrujula;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+@SuppressWarnings("deprecation")
+class Preview extends SurfaceView implements SurfaceHolder.Callback {
+	private static final String TAG = "Preview";
+
+	SurfaceHolder mHolder;
+	public Camera camera;
+
+	Preview(Context context) {
+		super(context);
+
+		// Install a SurfaceHolder.Callback so we get notified when the
+		// underlying surface is created and destroyed.
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+	
+	public void surfaceCreated(SurfaceHolder holder) {
+		// The Surface has been created, acquire the camera and tell it where
+		// to draw.
+		camera = getCameraInstance();
+		try {
+			camera.setDisplayOrientation(90);
+			camera.setPreviewDisplay(holder);
+
+			camera.setPreviewCallback(new PreviewCallback() {
+
+				public void onPreviewFrame(byte[] data, Camera arg1) {
+					FileOutputStream outStream = null;
+					try {
+						outStream = new FileOutputStream(String.format(
+								"/sdcard/%d.jpg", System.currentTimeMillis()));
+						outStream.write(data);
+						outStream.close();
+						Log.d(TAG, "onPreviewFrame - wrote bytes: "
+								+ data.length);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+					}
+					Preview.this.invalidate();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// Surface will be destroyed when we return, so stop the preview.
+		// Because the CameraDevice object is not a shared resource, it's very
+		// important to release it when the activity is paused.
+		camera.stopPreview();
+		camera = null;
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+		// Now that the size is known, set up the camera parameters and begin
+		// the preview.
+		Camera.Parameters parameters = camera.getParameters();
+		parameters.setPreviewSize(w, h);
+		camera.setParameters(parameters);
+		camera.startPreview();
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		super.draw(canvas);
+		Paint p = new Paint(Color.RED);
+		Log.d(TAG, "draw");
+		canvas.drawText("PREVIEW", canvas.getWidth() / 2,
+				canvas.getHeight() / 2, p);
+	}
+}
