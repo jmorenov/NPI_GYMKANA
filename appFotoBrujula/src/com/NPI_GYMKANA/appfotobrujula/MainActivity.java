e53d0f88d87896fd2ef88bec87f8cf1ebea9218a
Jmorenov <jmorenov28@gmail.com>
2015-01-21 09:07:49 +0100

    Probando.

--- appFotoBrujula/src/com/NPI_GYMKANA/appfotobrujula/MainActivity.java
+++ appFotoBrujula/src/com/NPI_GYMKANA/appfotobrujula/MainActivity.java
@@ -2,12 +2,15 @@ package com.NPI_GYMKANA.appfotobrujula;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_main);
+    	Toast toast = Toast.makeText(getApplicationContext(), "Primera prueba", Toast.LENGTH_SHORT);
+    	toast.show();
     }
 }
