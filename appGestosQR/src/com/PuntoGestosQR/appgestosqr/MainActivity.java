b011690eaf27c46c98a32765ac667b6fc076dccd
jmorenov <jmorenov28@gmail.com>
2015-01-28 23:03:32 +0100

    appGestosQR terminada.

--- appGestosQR/src/com/PuntoGestosQR/appgestosqr/MainActivity.java
+++ appGestosQR/src/com/PuntoGestosQR/appgestosqr/MainActivity.java
@@ -3,35 +3,30 @@
  *
  * This file is part of appGestosQR.
  *
- * MultiTouch is free software: you can redistribute it and/or modify
+ * appGestosQR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MultiTouch is distributed in the hope that it will be useful,
+ * appGestosQR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MultitTouch.  If not, see <http://www.gnu.org/licenses/>.
+ * along with appGestosQR.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.PuntoGestosQR.appgestosqr;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 /**
  * Clase MainActivity. Controla la ejecución de la app, llamando a la creación
@@ -44,26 +39,25 @@ import android.widget.Toast;
  */
 public class MainActivity extends Activity implements OnClickListener {
 
-	TextView lat;
-	TextView lon;
+	TextView result;
 
-    /**
-     * Called when the activity is starting. This is where most initialization
-     * should go: calling setContentView(int) to inflate the activity's UI,
-     * using findViewById(int) to programmatically interact with widgets in the
-     * UI, calling managedQuery(android.net.Uri, String[], String, String[],
-     * String) to retrieve cursors for data being displayed, etc.
-     * <p>
-     * You can call finish() from within this function, in which case
-     * onDestroy() will be immediately called without any of the rest of the
-     * activity lifecycle (onStart(), onResume(), onPause(), etc) executing.
-     *
-     *
-     * @param savedInstanceState
-     *            If the activity is being re-initialized after previously being
-     *            shut down then this Bundle contains the data it most recently
-     *            supplied in onSaveInstanceState(Bundle).
-     */
+	/**
+	 * Called when the activity is starting. This is where most initialization
+	 * should go: calling setContentView(int) to inflate the activity's UI,
+	 * using findViewById(int) to programmatically interact with widgets in the
+	 * UI, calling managedQuery(android.net.Uri, String[], String, String[],
+	 * String) to retrieve cursors for data being displayed, etc.
+	 * <p>
+	 * You can call finish() from within this function, in which case
+	 * onDestroy() will be immediately called without any of the rest of the
+	 * activity lifecycle (onStart(), onResume(), onPause(), etc) executing.
+	 *
+	 *
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle).
+	 */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -73,10 +67,16 @@ public class MainActivity extends Activity implements OnClickListener {
 		Button startButton = (Button) findViewById(R.id.button_start);
 		startButton.setOnClickListener(this);
 
-		lat = (TextView) findViewById(R.id.lat);
-		lon = (TextView) findViewById(R.id.lon);
+		result = (TextView) findViewById(R.id.result);
 	}
 
+	/**
+	 * Called when a view has been clicked.
+	 * 
+	 * @param view
+	 *            The view that was clicked.
+	 */
+	@Override
 	public void onClick(View v) {
 		if (v.getId() == R.id.button_start) {
 			Intent intent = new Intent(this, PatternActivity.class);
@@ -84,9 +84,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 
-    /**
-     *
-    */
+	/**
+	 * Inicia la ejecución del lector QR.
+	 *
+	 */
 	private void startQRReader() {
 		try {
 			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
@@ -99,44 +100,12 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 
-    /**
-     *
-     */
-	private float search(String s, String word) {
-		float f = 0F;
-		if (s.contains(word) || s.contains(word.toUpperCase()) || s.contains(word.toLowerCase())) {
-			String result = "";
-			for (int j = s.indexOf(word) + word.length(); j < s.length(); j++) {
-				char c;
-				if (Character.isDigit(s.charAt(j)) || s.charAt(j) == ','
-						|| s.charAt(j) == '.' || s.charAt(j) == '-') {
-					if (s.charAt(j) == ',')
-						c = '.';
-					else
-						c = s.charAt(j);
-					result += c;
-				} else if (s.charAt(j) != ' ') // Letra
-					break;
-			}
-			try {
-				f = Float.parseFloat(result);
-			} catch (NumberFormatException e) {
-				Log.e("ERROR", result);
-			}
-		}
-		return f;
-	}
-
-    /**
-     *
-     */
-	private void getLatLon(String data) {
-        lat.setText(data);
-	}
-    
-    /**
-     *
-     */
+	/**
+	 * Controla la ejecución de los intents llamados. requestCode 0 si la
+	 * activity era PatternActivity. requestCode 1 si la activity era el lector
+	 * QR de la librería ZXing.
+	 *
+	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
@@ -149,7 +118,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			}
 		} else if (requestCode == 1) {
 			if (resultCode == RESULT_OK) {
-				getLatLon(data.getStringExtra("SCAN_RESULT"));
+				result.setText(data.getStringExtra("SCAN_RESULT"));
 			}
 			if (resultCode == RESULT_CANCELED) {
 				// handle cancel
--- appGestosQR/src/com/PuntoGestosQR/appgestosqr/PatternActivity.java
+++ appGestosQR/src/com/PuntoGestosQR/appgestosqr/PatternActivity.java
@@ -3,18 +3,18 @@
  *
  * This file is part of appGestosQR.
  *
- * MultiTouch is free software: you can redistribute it and/or modify
+ * appGestosQR is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MultiTouch is distributed in the hope that it will be useful,
+ * appGestosQR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MultitTouch.  If not, see <http://www.gnu.org/licenses/>.
+ * along with appGestosQR.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.PuntoGestosQR.appgestosqr;
@@ -27,18 +27,13 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 
 /**
  * Clase PatternActivity. Controla los patrones a detectar.
@@ -51,9 +46,9 @@ import android.widget.Toast;
 public class PatternActivity extends Activity implements OnClickListener,
 		OnTouchListener {
 
-    /**
-    * Definición de los patrones.
-    */
+	/**
+	 * Definición de los patrones.
+	 */
 	ArrayList<ImageView> pattern = new ArrayList<ImageView>();
 	int[] ids = { R.id.pattern_1, R.id.pattern_2, R.id.pattern_3,
 			R.id.pattern_4, R.id.pattern_5, R.id.pattern_6, R.id.pattern_7,
@@ -66,34 +61,28 @@ public class PatternActivity extends Activity implements OnClickListener,
 	int[] pattern_N = { 6, 3, 0, 4, 8, 5, 2 };
 	int[] pattern_V = { 0, 7, 2 };
 
-	int [][] patterns = {pattern_Z,
-						pattern_L,
-						pattern_M,
-						pattern_N,
-						pattern_V};
-	String [] names = {"Z", "L", "M", "N", "V"};
-	
-	
+	int[][] patterns = { pattern_Z, pattern_L, pattern_M, pattern_N, pattern_V };
+	String[] names = { "Z", "L", "M", "N", "V" };
+
 	private static String TAG = "PatternActivity";
 
-            
-     /**
-        * Called when the activity is starting. This is where most initialization
-        * should go: calling setContentView(int) to inflate the activity's UI,
-        * using findViewById(int) to programmatically interact with widgets in the
-        * UI, calling managedQuery(android.net.Uri, String[], String, String[],
-        * String) to retrieve cursors for data being displayed, etc.
-        * <p>
-        * You can call finish() from within this function, in which case
-        * onDestroy() will be immediately called without any of the rest of the
-        * activity lifecycle (onStart(), onResume(), onPause(), etc) executing.
-        *
-        *
-        * @param savedInstanceState
-        *            If the activity is being re-initialized after previously being
-        *            shut down then this Bundle contains the data it most recently
-        *            supplied in onSaveInstanceState(Bundle).
-        */
+	/**
+	 * Called when the activity is starting. This is where most initialization
+	 * should go: calling setContentView(int) to inflate the activity's UI,
+	 * using findViewById(int) to programmatically interact with widgets in the
+	 * UI, calling managedQuery(android.net.Uri, String[], String, String[],
+	 * String) to retrieve cursors for data being displayed, etc.
+	 * <p>
+	 * You can call finish() from within this function, in which case
+	 * onDestroy() will be immediately called without any of the rest of the
+	 * activity lifecycle (onStart(), onResume(), onPause(), etc) executing.
+	 *
+	 *
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle).
+	 */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -109,26 +98,25 @@ public class PatternActivity extends Activity implements OnClickListener,
 		}
 	}
 
-            
-            /**
-             * Método que se llama cuando se detecta una pulsación en la pantalla.
-             *
-             * Implement this method to handle touch screen motion events.
-             *
-             * If this method is used to detect click actions, it is recommended that
-             * the actions be performed by implementing and calling performClick(). This
-             * will ensure consistent system behavior, including:
-             *
-             * - obeying click sound preferences - dispatching OnClickListener calls -
-             * handling ACTION_CLICK when accessibility features are enabled
-             *
-             * @param event
-             *            The motion event
-             *
-             * @return True if the event was handled, false otherwise.
-             *
-             */
-            
+	/**
+	 * Método que se llama cuando se detecta una pulsación en la pantalla.
+	 *
+	 * Implement this method to handle touch screen motion events.
+	 *
+	 * If this method is used to detect click actions, it is recommended that
+	 * the actions be performed by implementing and calling performClick(). This
+	 * will ensure consistent system behavior, including:
+	 *
+	 * - obeying click sound preferences - dispatching OnClickListener calls -
+	 * handling ACTION_CLICK when accessibility features are enabled
+	 *
+	 * @param event
+	 *            The motion event
+	 *
+	 * @return True if the event was handled, false otherwise.
+	 *
+	 */
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		if (pattern.contains(v)) {
@@ -147,6 +135,8 @@ public class PatternActivity extends Activity implements OnClickListener,
 			}
 		}
 
+		// ACTION_UP indica que se ha levantado el dedo de la pantalla, y por
+		// tanto damos por concluido el recorrido del patrón.
 		if (event.getAction() == MotionEvent.ACTION_UP) {
 			checkStatus();
 		}
@@ -154,19 +144,18 @@ public class PatternActivity extends Activity implements OnClickListener,
 		return false;
 	}
 
-            
-    /**
-    *
-    */
-	private void checkStatus()
-	{
+	/**
+	 * Comprueba la correción del patrón, y si este es correcto da por
+	 * finalizada la aplicación, si reinicia la lectura del patrón.
+	 *
+	 */
+	private void checkStatus() {
 		boolean finish = false;
 		Log.d(TAG, pattern_input.toString());
 		int color;
 		if (!checkPatterns())
 			color = Color.RED;
-		else
-		{
+		else {
 			color = Color.GREEN;
 			finish = true;
 		}
@@ -182,15 +171,14 @@ public class PatternActivity extends Activity implements OnClickListener,
 				cleanPattern();
 			}
 		}.start();
-		
-		if(finish)
-		{
+
+		if (finish) {
 			new CountDownTimer(500, 50) {
 				@Override
 				public void onTick(long arg0) {
 					// TODO Auto-generated method stub
 				}
-	
+
 				@Override
 				public void onFinish() {
 					Intent returnIntent = new Intent();
@@ -200,58 +188,61 @@ public class PatternActivity extends Activity implements OnClickListener,
 			}.start();
 		}
 	}
-	
-            
-    /**
-    *
-    */
+
+	/**
+	 * Comprueba si el recorrido del patrón realizado concuerda con alguno de
+	 * los patrones definidos.
+	 * 
+	 * @return true si el patrón es correcto
+	 * 
+	 * @see checkPattern
+	 */
 	private boolean checkPatterns() {
-		
-		for(int i=0; i<patterns.length; i++)
-		{
-			if(checkPattern(patterns[i], names[i]))
-			{
+
+		for (int i = 0; i < patterns.length; i++) {
+			if (checkPattern(patterns[i], names[i])) {
 				String result = "Patrón correcto: " + names[i];
 				Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
-            
-    /**
-    *
-    */
-	private boolean checkPattern(int [] pattern, String name)
-	{
-		if(pattern_input.size() == pattern.length)
-		{
+	/**
+	 * Comprueba si el recorrido del patrón realizado concuerda con pattern.
+	 *
+	 * @return true si el patrón es correcto.
+	 */
+	private boolean checkPattern(int[] pattern, String name) {
+		if (pattern_input.size() == pattern.length) {
 			for (int i = 0; i < pattern_input.size(); i++) {
-				if(pattern_input.get(i).intValue() != pattern[i])
+				if (pattern_input.get(i).intValue() != pattern[i])
 					return false;
 			}
 			return true;
 		}
 		return false;
 	}
-	
-            
-            
-    /**
-    *
-    */
+
+	/**
+	 * Colorea los puntos del patrón.
+	 *
+	 * @param c
+	 *            Color con el que se colorean los puntos del patrón.
+	 * 
+	 */
 	private void drawPattern(int c) {
 		for (int i = 0; i < pattern.size(); i++)
 			if (pattern_input.contains(new Integer(i)))
 				pattern.get(i).setColorFilter(c);
 	}
 
-            
-    /**
-    *
-    */
+	/**
+	 * Reinicia el color de los puntos del patrón.
+	 *
+	 */
 	private void cleanPattern() {
 		for (int i = 0; i < pattern.size(); i++)
 			pattern.get(i).setColorFilter(null);
@@ -261,10 +252,21 @@ public class PatternActivity extends Activity implements OnClickListener,
 	Rect outRect = new Rect();
 	int[] location = new int[2];
 
-            
-    /**
-    *
-    */
+	/**
+	 * Comprueba si la posición actual del toque en la pantalla pertenece a una
+	 * vista determinada.
+	 *
+	 * @param view
+	 *            Vista a comprobar.
+	 * @param x
+	 *            Posición x del punto.
+	 * @param y
+	 *            Posición y del punto.
+	 * 
+	 * @return true si la posición pertenece a la vista.
+	 * 
+	 * @see onTouch
+	 */
 	private boolean inViewInBounds(ImageView view, int x, int y) {
 		view.getDrawingRect(outRect);
 		view.getLocationOnScreen(location);
